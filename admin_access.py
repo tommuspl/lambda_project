ab0783d0d88c12251c63c683e389037a8384b9e3
tommuspl <74071885+tommuspl@users.noreply.github.com>
2021-11-25 10:46:36 +0100

    Update

--- admin_access.py
+++ admin_access.py
@@ -5,12 +5,21 @@ client = boto3.client('iam')
 
 
 def list_users_and_check_permissions(event, context):
-    
+    counter = 0
     users = client.list_users()['Users']
     
-    for user in users:
-        policies = client.list_attached_user_policies(UserName=user['UserName'])
-        for policy in policies['AttachedPolicies']:
-            if 'PolicyName' in policy and policy['PolicyName'] == 'AdministratorAccess':
-                client.detach_user_policy(UserName=user['UserName'],
-                                          PolicyArn='arn:aws:iam::aws:policy/AdministratorAccess')
+    if users is None:
+        return "No users found"
+    else:
+        for user in users:
+            print(f"User name: {user['UserName']} \n Policy: ")
+            policies = client.list_attached_user_policies(UserName=user['UserName'])
+            for policy in policies['AttachedPolicies']:
+                print(f"- {policy['PolicyName']:>5}")
+                if policy['PolicyName'] == 'AdministratorAccess':
+                    client.detach_user_policy(
+                        UserName=user['UserName'],
+                        PolicyArn='arn:aws:iam::aws:policy/AdministratorAccess'
+                        )
+                    counter +=1
+        return f"Detached {counter} policies"
